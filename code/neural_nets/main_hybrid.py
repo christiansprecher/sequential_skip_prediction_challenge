3048930b89be740074459490a9f06f1d481c6804
christiansprecher <cspreche@ethz.ch>
2018-12-31 11:48:18 +0100

    add multiple merge options + add simple rnn model + add gridsearch for model evaluation

--- code/neural_nets/main_hybrid.py
+++ code/neural_nets/main_hybrid.py
@@ -7,7 +7,7 @@ import glob
 import time
 from tensorflow.python.client import device_lib
 
-from models import Hybrid
+from models import Hybrid, Single_RNN_Full
 import utils
 
 
@@ -36,15 +36,16 @@ def run_local_test():
     del x_rnn, x_fc, y
 
     # Generate model
-    rnn_layer_sizes = np.array([128, 32, 32])
-    dense_layer_parallel_sizes = np.array([32, 32])
-    dense_layer_sequential_sizes = np.array([32, 20, 1])
+    rnn_layer_sizes = np.array([128, 32])
+    dense_layer_parallel_sizes = np.array([32])
+    dense_layer_sequential_sizes = np.array([20, 1])
     # rnn_layer_sizes = np.array([ 32])
     # dense_layer_parallel_sizes = np.array([32])
     # dense_layer_sequential_sizes = np.array([1])
-    dropout_prob_rnn = 0.3
-    dropout_prob_dense = 0.3
-    lambda_reg_dense = 0.001
+    dropout_prob_rnn = 0
+    dropout_prob_dense = 0
+    lambda_reg_dense = 0.00
+    lambda_reg_rnn = 0.00
 
     model = Hybrid()
     model.build_model(
@@ -53,9 +54,22 @@ def run_local_test():
         dense_layer_sequential_sizes = dense_layer_sequential_sizes,
         dropout_prob_rnn = dropout_prob_rnn,
         dropout_prob_dense = dropout_prob_dense,
-        lambda_reg_dense = lambda_reg_dense)
+        lambda_reg_dense = lambda_reg_dense,
+        lambda_reg_rnn = lambda_reg_rnn,
+        merge = 'maximum')
 
-    # model.plot_model()
+    # model = Single_RNN_Full()
+    # model.build_model(
+    #     rnn_layer_sizes = rnn_layer_sizes,
+    #     dense_layer_sequential_sizes = dense_layer_sequential_sizes,
+    #     dropout_prob_rnn = dropout_prob_rnn,
+    #     dropout_prob_dense = dropout_prob_dense,
+    #     lambda_reg_dense = lambda_reg_dense,
+    #     lambda_reg_rnn = lambda_reg_rnn)
+
+    model.compile(optimizer = 'Adam', loss = 'hinge')
+
+    model.plot_model()
     model.print_summary()
 
     model.fit(x_rnn_train, x_fc_train, y_train, x_rnn_valid,
@@ -65,7 +79,6 @@ def run_local_test():
 
     model.evaluate(x_rnn_test, x_fc_test, y_test, verbosity=2)
     # model.predict(x_rnn_test, x_fc_test, write_to_file = True)
-
 
 def run_on_server_simple():
     print(device_lib.list_local_devices())
@@ -131,6 +144,7 @@ def run_on_server_simple():
         dropout_prob_rnn = dropout_prob_rnn,
         dropout_prob_dense = dropout_prob_dense,
         lambda_reg_dense = lambda_reg_dense)
+    model.compile()
 
     model.print_summary()
 
@@ -189,7 +203,6 @@ def predict_on_server_simple():
     end = time.process_time()
     print("All files written, time used: %4.2f seconds" % (end-start))
 
-
 def run_on_server_using_generator():
 
     start = time.process_time()
@@ -210,6 +223,7 @@ def run_on_server_using_generator():
         dropout_prob_rnn = dropout_prob_rnn,
         dropout_prob_dense = dropout_prob_dense,
         lambda_reg_dense = lambda_reg_dense)
+    model.compile()
 
     model.print_summary()
 
@@ -222,8 +236,6 @@ def run_on_server_using_generator():
 
     end = time.process_time()
     print("Model trained, time used: %4.2f seconds" % (end-start))
-
-
 
 def run_local_using_generator():
 
@@ -245,6 +257,7 @@ def run_local_using_generator():
         dropout_prob_rnn = dropout_prob_rnn,
         dropout_prob_dense = dropout_prob_dense,
         lambda_reg_dense = lambda_reg_dense)
+    model.compile()
 
     model.print_summary()
 
@@ -258,6 +271,157 @@ def run_local_using_generator():
     end = time.process_time()
     print("Model trained, time used: %4.2f seconds" % (end-start))
 
+def grid_search(x_rnn_train, x_fc_train, y_train, x_rnn_valid,
+        x_fc_valid, y_valid, x_rnn_test, x_fc_test, y_test,
+        batch_size = 64, epochs = 50):
+
+    rnn_layer_sizes = np.array([32])
+    dense_layer_parallel_sizes = np.array([32])
+    dense_layer_sequential_sizes = np.array([16, 1])
+    dropout_prob_rnn = 0.1
+    dropout_prob_dense = 0.1
+    lambda_reg_dense = 0.001
+    lambda_reg_rnn = 0.001
+
+    lr_range = [0.1, 0.01, 0.001, 0.0001]
+    optimizer_range = ['SGD', 'Adam']
+    loss_range = ['s_hinge','m_hinge_acc']
+    merge_range = ['multiply', 'add', 'concatenate', 'maximum']
+
+    for lr in lr_range:
+        for optimizer in optimizer_range:
+            for loss in loss_range:
+                for merge in merge_range:
+
+                    model = Hybrid(model_name = 'hybrid_' + merge)
+
+                    model.build_model(
+                        rnn_layer_sizes = rnn_layer_sizes,
+                        dense_layer_parallel_sizes = dense_layer_parallel_sizes,
+                        dense_layer_sequential_sizes = dense_layer_sequential_sizes,
+                        dropout_prob_rnn = dropout_prob_rnn,
+                        dropout_prob_dense = dropout_prob_dense,
+                        lambda_reg_dense = lambda_reg_dense,
+                        lambda_reg_rnn = lambda_reg_rnn,
+                        merge = merge)
+
+                    model.compile(optimizer = optimizer, loss = loss, lr = lr)
+
+                    model.fit(x_rnn_train, x_fc_train, y_train, x_rnn_valid,
+                        x_fc_valid, y_valid,
+                        epochs = epochs, batch_size = batch_size,
+                        verbosity = 0, patience = 10)
+                    model.plot_training()
+                    model.save_model()
+
+                    print('Model: %s,' % model.model_name,
+                        ' with loss = %s,' % loss,
+                        ' opt = %s,' % optimizer,
+                        ' lr = %4.2f' % lr)
+
+                    model.evaluate(x_rnn_test, x_fc_test, y_test, verbosity=2)
+
+                model = Single_RNN_Full()
+                model.build_model(
+                    rnn_layer_sizes = rnn_layer_sizes,
+                    dense_layer_sequential_sizes = dense_layer_sequential_sizes,
+                    dropout_prob_rnn = dropout_prob_rnn,
+                    dropout_prob_dense = dropout_prob_dense,
+                    lambda_reg_dense = lambda_reg_dense,
+                    lambda_reg_rnn = lambda_reg_rnn)
+
+                model.compile(optimizer = optimizer, loss = loss, lr = lr)
+
+                model.fit(x_rnn_train, x_fc_train, y_train, x_rnn_valid,
+                    x_fc_valid, y_valid, epochs=50, batch_size = 64,
+                    verbosity=0, patience = 10)
+                model.plot_training()
+                model.save_model()
+
+                print('Model: %s,' % model.model_name,
+                    ' with loss = %s,' % loss,
+                    ' opt = %s,' % optimizer,
+                    ' lr = %4.2f' % lr)
+
+                model.evaluate(x_rnn_test, x_fc_test, y_test, verbosity=2)
+
+
+def run_on_server_grid_search():
+    start = time.process_time()
+    path = '/cluster/scratch/cspreche/spotify_challenge'
+
+    train_path = path + '/training_set_preproc'
+    test_path = path + '/test_set_preproc'
+    submission_path = path + '/submissions'
+
+    tracks_path = train_path + '/log_8_20180902_000000000000.csv'
+    sessions_path = train_path + '/session_log_8_20180902_000000000000.csv'
+
+    x_rnn, x_fc, y = utils.load_training_data_simple(tracks_path, sessions_path)
+
+    s = x_rnn.shape[0]
+    shuffle_indices = np.random.permutation(np.arange(s))
+    indices_train = shuffle_indices[:int(s*0.65)]
+    indices_valid = shuffle_indices[int(s*0.65):int(s*0.75)]
+    indices_test = shuffle_indices[int(s*0.75):]
+
+    x_rnn_train = x_rnn[indices_train,:,:]
+    x_fc_train = x_fc[indices_train,:]
+    y_train = y[indices_train,:]
+
+    x_rnn_valid = x_rnn[indices_valid,:,:]
+    x_fc_valid = x_fc[indices_valid,:]
+    y_valid = y[indices_valid,:]
+
+    x_rnn_test = x_rnn[indices_test,:,:]
+    x_fc_test = x_fc[indices_test,:]
+    y_test = y[indices_test,:]
+
+    del x_rnn, x_fc, y
+
+    batch_size = 64
+    epochs = 50
+
+    grid_search(x_rnn_train, x_fc_train, y_train, x_rnn_valid,
+            x_fc_valid, y_valid, x_rnn_test, x_fc_test, y_test,
+            batch_size = batch_size, epochs = epochs)
+
+    end = time.process_time()
+    print("Time used: %4.2f seconds" % (end-start))
+
+
+def run_local_grid_search():
+    start = time.process_time()
+    x_rnn, x_fc, y = utils.load_training_data_simple()
+
+    # Generate validation set
+    s = x_rnn.shape[0]
+    shuffle_indices = np.random.permutation(np.arange(s))
+    indices_train = shuffle_indices[:int(s*0.5)]
+    indices_valid = shuffle_indices[int(s*0.5):int(s*0.75)]
+    indices_test = shuffle_indices[int(s*0.75):]
+
+    x_rnn_train = x_rnn[indices_train,:,:]
+    x_fc_train = x_fc[indices_train,:]
+    y_train = y[indices_train,:]
+
+    x_rnn_valid = x_rnn[indices_valid,:,:]
+    x_fc_valid = x_fc[indices_valid,:]
+    y_valid = y[indices_valid,:]
+
+    x_rnn_test = x_rnn[indices_test,:,:]
+    x_fc_test = x_fc[indices_test,:]
+    y_test = y[indices_test,:]
+
+    del x_rnn, x_fc, y
+
+    # Generate model
+    batch_size = 16
+    epochs = 5
+
+    grid_search(x_rnn_train, x_fc_train, y_train, x_rnn_valid,
+            x_fc_valid, y_valid, x_rnn_test, x_fc_test, y_test,
+            batch_size = batch_size, epochs = epochs)
 
 
 if __name__ == '__main__':
@@ -281,6 +445,12 @@ if __name__ == '__main__':
 
     elif task == '4':
         run_local_using_generator()
+
+    elif task == '5':
+        run_local_grid_search()
+
+    elif task == '6':
+        run_on_server_grid_search()
 
     else:
         print('Choose Task')
--- code/neural_nets/models.py
+++ code/neural_nets/models.py
@@ -2,7 +2,7 @@ import numpy as np
 import keras
 from keras.models import Sequential, Model as K_Model, save_model, load_model
 from keras.layers import (Input, Dense, Dropout, Flatten, Concatenate, Reshape,
-    GRU, LSTM, concatenate, multiply)
+    GRU, LSTM, RepeatVector, Permute, concatenate, multiply, maximum, add)
 from keras.optimizers import SGD, Adam
 from keras.layers.normalization import BatchNormalization
 from keras.callbacks import EarlyStopping, ModelCheckpoint
@@ -102,7 +102,6 @@ class Model:
                 yield ({'tracks_input': batch_x_rnn, 'session_input': batch_x_fc},
                 {'output': batch_y})
 
-
     def generate_test_data(self, path):
 
         tracks_logs = sorted(glob.glob(path + "/test_set_preproc/log_*.csv"))
@@ -118,150 +117,31 @@ class Model:
             idx = (idx + 1 ) % n
             yield ({'tracks_input': x_rnn, 'session_input': x_fc})
 
-    def fit(self):
-        pass
+    def compile(self, optimizer = 'Adam', loss = 's_hinge', lr = 0.001):
+        optimizer_v = []
+        if optimizer == 'Adam':
+            optimizer_v = Adam(lr = lr)
+        elif optimizer == 'SGD':
+            optimizer_v = SGD(lr = lr)
 
-    def fit_generator(self):
-        pass
+        if loss == 's_hinge':
+            self.model.compile(optimizer = optimizer_v, loss = s_hinge,
+                metrics=[ns_binary_acc, avg_mean_acc, fp_acc])
 
-    def evaluate(self):
-        pass
-
-    def predict(self):
-        pass
-
-    def predict_generator(self):
-        pass
-
-    def save_model(self):
-        path = self.path + self.model_name + '_' + self.now + '.h5'
-        save_model(self.model,path,overwrite=True,include_optimizer=True)
-
-    def load_model(self, name):
-        path = self.path + name + '.h5'
-        self.model = load_model(path,
-            custom_objects={'selective_hinge': s_hinge,
-            'mean_hinge_accuracy' : m_hinge_acc,
-            'normed_selective_binary_accuracy' : ns_binary_acc,
-            'average_mean_accuracy' : avg_mean_acc,
-            'first_prediction_accuracy' : fp_acc})
-
-    def plot_model(self):
-        path = self.path + self.model_name + '_' + self.now + '_architecture.png'
-        plot_model(self.model, to_file=path)
-
-    def print_summary(self):
-        print(self.model.summary())
-
-    def plot_training(self):
-        #Extract the metrics from the history
-        keys = self.history.history.keys()
-        for key in keys:
-            if key.find('val_') > -1 and key.find('val_loss') == -1:
-                # Plot training & validation accuracy values
-                plt.plot(self.history.history[key[4:]])
-                plt.plot(self.history.history[key])
-                plt.title('Model accuracy')
-                plt.ylabel('Accuracy')
-                plt.xlabel('Epoch')
-                plt.legend(['Train', 'Test'], loc='upper left')
-                plot_name = (self.path + self.model_name + '_'
-                    + self.now + '_' + key[4:] + '.png')
-                plt.savefig(plot_name, bbox_inches='tight')
-                plt.clf()
-
-
-        # Plot training & validation loss values
-        plt.plot(self.history.history['loss'])
-        plt.plot(self.history.history['val_loss'])
-        plt.title('Model loss')
-        plt.ylabel('Loss')
-        plt.xlabel('Epoch')
-        plt.legend(['Train', 'Test'], loc='upper left')
-        plot_name = (self.path + self.model_name + '_'
-            + self.now + '_loss.png')
-        plt.savefig(plot_name, bbox_inches='tight')
-
-
-#hybrid models
-class Hybrid(Model):
-
-    def __init__(self, model_name = 'hybrid'):
-        self.model_name = model_name
-        self.set_shape()
-        self.create_folder()
-        self.set_timestamp()
-
-    def build_model(self,
-        rnn_layer_sizes = np.array([20, 20, 20]),
-        dense_layer_parallel_sizes = np.array([10, 10]),
-        dense_layer_sequential_sizes = np.array([32, 20]),
-        dropout_prob_rnn = 0.3,
-        dropout_prob_dense = 0.3,
-        lambda_reg_dense = 0.001):
-
-        self.rnn_layer_sizes = rnn_layer_sizes
-        self.dense_layer_parallel_sizes = dense_layer_parallel_sizes
-        self.dense_layer_sequential_sizes = dense_layer_sequential_sizes
-        self.dropout_prob_rnn = dropout_prob_rnn
-        self.dropout_prob_dense = dropout_prob_dense
-        self.lambda_reg_dense = lambda_reg_dense
-
-        if dense_layer_parallel_sizes[-1] != rnn_layer_sizes[-1]:
-            print('Dimensions of last layers of RNN and of parallel dense network must agree!')
-            return
-
-        # define inputs
-        tracks_input = Input(self.input_shape_tracks , dtype='float32', name='tracks_input')
-        session_input = Input(self.input_shape_sessions , dtype='float32', name='session_input')
-
-        # RNN side
-        x_rnn = LSTM(self.rnn_layer_sizes[0],return_sequences=True)(tracks_input)
-        for i in range(1, self.rnn_layer_sizes.size):
-            x_rnn = LSTM(self.rnn_layer_sizes[i],return_sequences=True)(x_rnn)
-
-        x_rnn = BatchNormalization()(x_rnn)
-        out_rnn = Dropout(self.dropout_prob_rnn)(x_rnn)
-
-        # dense side
-        x_fc = Dense(self.dense_layer_parallel_sizes[0], activation='relu',
-            kernel_regularizer=l2(self.lambda_reg_dense))(session_input)
-
-        for i in range(1, self.dense_layer_parallel_sizes.size):
-            x_fc = Dense(self.dense_layer_parallel_sizes[i], activation='relu',
-                kernel_regularizer=l2(self.lambda_reg_dense))(x_fc)
-
-        x_fc = BatchNormalization()(x_fc)
-        out_fc = Dropout(self.dropout_prob_dense)(x_fc)
-
-        # merge RNN and dense side
-        x = multiply([out_rnn, out_fc])
-
-        for i in range(self.dense_layer_sequential_sizes.size - 1):
-            x = Dense(self.dense_layer_sequential_sizes[i], activation='relu',
-                kernel_regularizer=l2(self.lambda_reg_dense))(x)
-
-        x = Dense(self.dense_layer_sequential_sizes[-1], activation='linear',
-            kernel_regularizer=l2(self.lambda_reg_dense))(x)
-
-        output = Reshape(self.output_shape, name = 'output')(x)
-
-        # create model and compile it
-        self.model = K_Model(inputs=[tracks_input, session_input], outputs=[output])
-
-        self.compile()
-
-    def compile(self):
-        self.model.compile(optimizer='Adam', loss=m_hinge_acc,
-            metrics=[ns_binary_acc, avg_mean_acc, fp_acc])
+        elif loss == 'm_hinge_acc':
+            self.model.compile(optimizer = optimizer_v, loss = m_hinge_acc,
+                metrics=[ns_binary_acc, avg_mean_acc, fp_acc])
+        else:
+            self.model.compile(optimizer = optimizer_v, loss = loss,
+                metrics=[ns_binary_acc, avg_mean_acc, fp_acc])
 
     def fit(self, x_train_rnn, x_train_fc, y_train, x_valid_rnn = None,
-        x_valid_fc = None, y_valid = None, epochs=50, batch_size=64,
-        patience = 5, verbosity = 0):
+            x_valid_fc = None, y_valid = None, epochs=50, batch_size=64,
+            patience = 5, verbosity = 0):
 
         # define callbacks
         self.callbacks = [EarlyStopping(monitor='val_loss', patience=patience),
-             ModelCheckpoint(filepath=self.path + self.model_name + '_{epoch:02d}_{val_loss:.4f}.h5',
+             ModelCheckpoint(filepath=self.path + self.model_name + '_{epoch:02d}_{val_average_mean_accuracy:.4f}.h5',
                 monitor='val_loss', save_best_only=False)]
 
         self.history = self.model.fit({'tracks_input': x_train_rnn, 'session_input': x_train_fc},
@@ -321,3 +201,191 @@ class Hybrid(Model):
                 path = '../../data/submissions'
             path = path + '/' + self.model_name + '_' + self.now + '.txt'
             utils.save_submission(y_pred,session_length, path, overwrite = overwrite)
+
+    def save_model(self):
+        path = self.path + self.model_name + '_' + self.now + '.h5'
+        save_model(self.model,path,overwrite=True,include_optimizer=True)
+
+    def load_model(self, name):
+        path = self.path + name + '.h5'
+        self.model = load_model(path,
+            custom_objects={'selective_hinge': s_hinge,
+            'mean_hinge_accuracy' : m_hinge_acc,
+            'normed_selective_binary_accuracy' : ns_binary_acc,
+            'average_mean_accuracy' : avg_mean_acc,
+            'first_prediction_accuracy' : fp_acc})
+
+    def plot_model(self):
+        path = self.path + self.model_name + '_' + self.now + '_architecture.png'
+        plot_model(self.model, to_file=path)
+
+    def print_summary(self):
+        print(self.model.summary())
+
+    def plot_training(self):
+        #Extract the metrics from the history
+        keys = self.history.history.keys()
+        for key in keys:
+            if key.find('val_') > -1 and key.find('val_loss') == -1:
+                # Plot training & validation accuracy values
+                plt.plot(self.history.history[key[4:]])
+                plt.plot(self.history.history[key])
+                plt.title('Model accuracy')
+                plt.ylabel('Accuracy')
+                plt.xlabel('Epoch')
+                plt.legend(['Train', 'Test'], loc='upper left')
+                plot_name = (self.path + self.model_name + '_'
+                    + self.now + '_' + key[4:] + '.png')
+                plt.savefig(plot_name, bbox_inches='tight')
+                plt.clf()
+
+
+        # Plot training & validation loss values
+        plt.plot(self.history.history['loss'])
+        plt.plot(self.history.history['val_loss'])
+        plt.title('Model loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Test'], loc='upper left')
+        plot_name = (self.path + self.model_name + '_'
+            + self.now + '_loss.png')
+        plt.savefig(plot_name, bbox_inches='tight')
+
+
+# hybrid models
+class Hybrid(Model):
+
+    def __init__(self, model_name = 'hybrid'):
+        self.model_name = model_name
+        self.set_shape()
+        self.create_folder()
+        self.set_timestamp()
+
+    def build_model(self,
+        rnn_layer_sizes = np.array([20, 20, 20]),
+        dense_layer_parallel_sizes = np.array([10, 20]),
+        dense_layer_sequential_sizes = np.array([32, 20]),
+        dropout_prob_rnn = 0.3,
+        dropout_prob_dense = 0.3,
+        lambda_reg_rnn = 0.001,
+        lambda_reg_dense = 0.001,
+        merge = 'multiply'):
+
+        self.rnn_layer_sizes = rnn_layer_sizes
+        self.dense_layer_parallel_sizes = dense_layer_parallel_sizes
+        self.dense_layer_sequential_sizes = dense_layer_sequential_sizes
+        self.dropout_prob_rnn = dropout_prob_rnn
+        self.dropout_prob_dense = dropout_prob_dense
+        self.lambda_reg_rnn = lambda_reg_rnn
+        self.lambda_reg_dense = lambda_reg_dense
+        self.merge = merge
+
+        if dense_layer_parallel_sizes[-1] != rnn_layer_sizes[-1]:
+            print('Dimensions of last layers of RNN and of parallel dense network must agree!')
+            return
+
+        # define inputs
+        tracks_input = Input(self.input_shape_tracks , dtype='float32', name='tracks_input')
+        session_input = Input(self.input_shape_sessions , dtype='float32', name='session_input')
+
+        # RNN side
+        x_rnn = LSTM(self.rnn_layer_sizes[0],return_sequences=True,
+            kernel_regularizer=l2(self.lambda_reg_rnn))(tracks_input)
+        for i in range(1, self.rnn_layer_sizes.size):
+            x_rnn = LSTM(self.rnn_layer_sizes[i],return_sequences=True,
+            kernel_regularizer=l2(self.lambda_reg_rnn))(x_rnn)
+
+        x_rnn = BatchNormalization()(x_rnn)
+        out_rnn = Dropout(self.dropout_prob_rnn)(x_rnn)
+
+        # dense side
+        x_fc = Dense(self.dense_layer_parallel_sizes[0], activation='relu',
+            kernel_regularizer=l2(self.lambda_reg_dense))(session_input)
+
+        for i in range(1, self.dense_layer_parallel_sizes.size):
+            x_fc = Dense(self.dense_layer_parallel_sizes[i], activation='relu',
+                kernel_regularizer=l2(self.lambda_reg_dense))(x_fc)
+
+        x_fc = BatchNormalization()(x_fc)
+        out_fc = Dropout(self.dropout_prob_dense)(x_fc)
+
+        x = []
+        # merge RNN and dense side
+        if self.merge == 'multiply':
+            x = multiply([out_rnn, out_fc])
+        elif self.merge == 'add':
+            out_fc = RepeatVector(20)(out_fc)
+            x = add([out_rnn, out_fc])
+        elif self.merge == 'concatenate':
+            out_fc = RepeatVector(20)(out_fc)
+            x = concatenate([out_rnn, out_fc], axis = -1)
+        elif self.merge == 'maximum':
+            out_fc = RepeatVector(20)(out_fc)
+            x = maximum([out_rnn, out_fc])
+        else:
+            print('Choose proper merge variation: multiply, add, concatenate or maximum')
+
+        for i in range(self.dense_layer_sequential_sizes.size - 1):
+            x = Dense(self.dense_layer_sequential_sizes[i], activation='relu',
+                kernel_regularizer=l2(self.lambda_reg_dense))(x)
+
+        x = Dense(self.dense_layer_sequential_sizes[-1], activation='linear',
+            kernel_regularizer=l2(self.lambda_reg_dense))(x)
+
+        output = Reshape(self.output_shape, name = 'output')(x)
+
+        # create model and compile it
+        self.model = K_Model(inputs=[tracks_input, session_input], outputs=[output])
+
+class Single_RNN_Full(Model):
+
+    def __init__(self, model_name = 'single_rnn_full'):
+        self.model_name = model_name
+        self.set_shape()
+        self.create_folder()
+        self.set_timestamp()
+
+    def build_model(self,
+        rnn_layer_sizes = np.array([20, 20, 20]),
+        dense_layer_sequential_sizes = np.array([32, 20]),
+        dropout_prob_rnn = 0.3,
+        dropout_prob_dense = 0.3,
+        lambda_reg_rnn = 0.001,
+        lambda_reg_dense = 0.001):
+
+        self.rnn_layer_sizes = rnn_layer_sizes
+        self.dense_layer_sequential_sizes = dense_layer_sequential_sizes
+        self.dropout_prob_rnn = dropout_prob_rnn
+        self.dropout_prob_dense = dropout_prob_dense
+        self.lambda_reg_rnn = lambda_reg_rnn
+        self.lambda_reg_dense = lambda_reg_dense
+
+        # define inputs
+        tracks_input = Input(self.input_shape_tracks , dtype='float32', name='tracks_input')
+        session_input = Input(self.input_shape_sessions , dtype='float32', name='session_input')
+
+        # Concatenate sessions and tracks
+        session_rep = RepeatVector(20)(session_input)
+        x_input = concatenate([tracks_input, session_rep], axis = -1)
+
+        # RNN part
+        x = LSTM(self.rnn_layer_sizes[0],return_sequences=True,
+            kernel_regularizer=l2(self.lambda_reg_rnn))(x_input)
+        for i in range(1, self.rnn_layer_sizes.size):
+            x = LSTM(self.rnn_layer_sizes[i],return_sequences=True,
+            kernel_regularizer=l2(self.lambda_reg_rnn))(x)
+
+        x = BatchNormalization()(x)
+        x = Dropout(self.dropout_prob_rnn)(x)
+
+        for i in range(self.dense_layer_sequential_sizes.size - 1):
+            x = Dense(self.dense_layer_sequential_sizes[i], activation='relu',
+                kernel_regularizer=l2(self.lambda_reg_dense))(x)
+
+        x = Dense(self.dense_layer_sequential_sizes[-1], activation='linear',
+            kernel_regularizer=l2(self.lambda_reg_dense))(x)
+
+        output = Reshape(self.output_shape, name = 'output')(x)
+
+        # create model
+        self.model = K_Model(inputs=[tracks_input, session_input], outputs=[output])
